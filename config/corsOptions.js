139e7e3d920627e62cdb032a8f515a242e4f0d59
Yao J <kimmyoo@gmail.com>
2023-08-12 16:01:03 -0400

    changed corsOption

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -3,20 +3,18 @@ import allowedOrigins from "./allowedOrigins.js"
 const corsOptions = {
     origin: (origin, callback) => {
         // for production
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            // throw error
-            callback(new Error('Not allowed by CORS'))
-        }
-        if (allowedOrigins.includes(origin)) {
-            res.setHeader('Access-Control-Allow-Origin', origin);
-        }
-        // this is for development using postman
         // if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        //     // allow
-        //     callback(null, true)
-        // } else {
+        //     // throw error
         //     callback(new Error('Not allowed by CORS'))
         // }
+
+        // this is for development using postman
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            // allow
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
     },
 
     credentials: true,
