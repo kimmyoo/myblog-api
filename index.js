7e7ffa6a560c9143e05e7fb637a11c8e2b9d75ae
Yao J <kimmyoo@gmail.com>
2023-08-19 11:55:58 -0400

    added app.set('trust proxy', 1) in index.js for rate limit setup on render.com

--- index.js
+++ index.js
@@ -50,6 +50,9 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(helmet())
 app.use(morgan("common"))
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+app.set('trust proxy', 1);
 
 /* Parse incoming request bodies in a 
 middleware before your handlers, 
